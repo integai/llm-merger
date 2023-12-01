e345d3947218ff3e812aa444bd6fe9e3bffee859
oblvisheee <molniya213y@gmail.com>
2023-12-01 23:25:02 +0300

    Test fix.

--- merger.py
+++ merger.py
@@ -3,62 +3,52 @@ import torch
 import copy
 from safetensors.torch import load_file, save_file
 
-class ModelMerger:
-    def __init__(self):
-        self.args = self.parse_args()
+def parse_args():
+    parser = argparse.ArgumentParser(description='Merge two models')
+    parser.add_argument('--model1', type=str, required=True, help='Path to the first model')
+    parser.add_argument('--model2', type=str, required=True, help='Path to the second model')
+    parser.add_argument('--output', type=str, required=True, help='Path to save the merged model')
+    parser.add_argument('--range', type=float, nargs=2, default=[50, 50], help='Range between model1 and model2')
+    return parser.parse_args()
 
-    def parse_args(self):
-        parser = argparse.ArgumentParser(description='Merge two models')
-        parser.add_argument('--model1', type=str, required=True, help='Path to the first model')
-        parser.add_argument('--model2', type=str, required=True, help='Path to the second model')
-        parser.add_argument('--output', type=str, required=True, help='Path to save the merged model')
-        parser.add_argument('--range', type=float, nargs=2, default=[50, 50], help='Range between model1 and model2')
-        return parser.parse_args()
+def load_model(model_path):
+    state_dict = load_file(model_path) if model_path.endswith('.safetensors') else torch.load(model_path)
+    model = torch.nn.Module()
+    model.load_state_dict(state_dict)
+    model.eval()
+    return model
 
-    def load_model(self, model_path):
-        # Define your model architecture here
-        model_architecture = MyModel()  # Replace 'MyModel' with your actual model class
+def merge_models(model1, model2, range):
+    if type(model1) != type(model2):
+        raise ValueError("Models must be of the same class")
 
-        if model_path.endswith('.safetensors'):
-            state_dict = load_file(model_path)
-        else:
-            state_dict = torch.load(model_path)
-    
-        model_architecture.load_state_dict(state_dict)
-        model_architecture.eval()
-        return model_architecture
+    merged = copy.deepcopy(model1)
 
-    def merge_models(self, model1, model2, range):
-        if type(model1) != type(model2):
-            raise ValueError("Models must be of the same class")
+    with torch.no_grad():
+        for (key1, param1), (key2, param2) in zip(model1.named_parameters(), model2.named_parameters()):
+            if key1 != key2:
+                raise ValueError("Models have different architectures")
+            merged.state_dict()[key1].copy_((param1 * (1 - range[0]/100) + param2 * range[1]/100))
 
-        merged = copy.deepcopy(model1)
+    return merged
 
-        with torch.no_grad():
-            for (key1, param1), (key2, param2) in zip(model1.named_parameters(), model2.named_parameters()):
-                if key1 != key2:
-                    raise ValueError("Models have different architectures")
-                merged.state_dict()[key1].copy_((param1 * (1 - range[0]/100) + param2 * range[1]/100))
+def save_model(model, path):
+    if not path.endswith('.safetensors') and not path.endswith('.pth'):
+        path = path.rsplit('.', 1)[0] + '.pth'
+    if path.endswith('.safetensors'):
+        save_file(model, path)
+    else:
+        torch.save(model.state_dict(), path)
 
-        return merged
+def run():
+    args = parse_args()
+    model1 = load_model(args.model1)
+    model2 = load_model(args.model2)
 
-    def save_model(self, model, path):
-        if not path.endswith('.safetensors') and not path.endswith('.pth'):
-            path = path.rsplit('.', 1)[0] + '.pth'
-        if path.endswith('.safetensors'):
-            save_file(model, path)
-        else:
-            torch.save(model.state_dict(), path)
-            
+    merged_model = merge_models(model1, model2, args.range)
 
-    def run(self):
-        model1 = self.load_model(self.args.model1)
-        model2 = self.load_model(self.args.model2)
-
-        merged_model = self.merge_models(model1, model2, self.args.range)
-
-        self.save_model(merged_model, self.args.output)
+    save_model(merged_model, args.output)
 
 if __name__ == '__main__':
-    merger = ModelMerger()
-    merger.run()
+    run()
+
